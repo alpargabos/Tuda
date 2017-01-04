95714bf91da6ce71cdd459502a16dc872e8922cc
Alpar Gabos <alpar.gabos@prezi.com>
2017-01-04 21:32:45 +0100

    moments detail skeleton

--- app/src/main/java/com/alpargabos/tuda/moments/MomentsAdapter.java
+++ app/src/main/java/com/alpargabos/tuda/moments/MomentsAdapter.java
@@ -1,14 +1,15 @@
 package com.alpargabos.tuda.moments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.MediaMetadataRetriever;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.alpargabos.tuda.R;
+import com.alpargabos.tuda.moments.details.MomentsDetailActivity;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
@@ -38,7 +39,7 @@ public class MomentsAdapter extends RecyclerView.Adapter<MomentsViewHolder> {
 	}
 
 	@Override
-	public void onBindViewHolder(final MomentsViewHolder holder, int position) {
+	public void onBindViewHolder(final MomentsViewHolder holder, final int position) {
 		final ImportantMoment moment = importantMoments.get(position);
 		if (moment.getType() == ImportantMoment.VIDEO) {
 			holder.videoMark.setVisibility(View.VISIBLE);
@@ -64,7 +65,9 @@ public class MomentsAdapter extends RecyclerView.Adapter<MomentsViewHolder> {
 		holder.thumbnail.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Snackbar.make(view, "Card wit url " + moment.getUrl() + " was clicked!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+				Intent intent = new Intent(context, MomentsDetailActivity.class);
+				intent.putExtra("pos", position);
+				context.startActivity(intent);
 			}
 		});
 	}
